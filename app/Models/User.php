161df592d785b9bbbad00fe67cd019e917b08c34
Yury Videnin <yuryhhp@gmail.com>
2023-07-19 17:55:24 +0300

    added user migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\User as Authenticatable;   
+use Illuminate\Foundation\Auth\User as Authenticatable; 
+use Illuminate\Notifications\Notifiable;  
 
 class User extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+
+    protected  $fillable = [ 
+        'name', 
+        'email', 
+        'password',
+    ]; 
+
+    protected $hidden = [ 
+        'password'
+    ];
 }
