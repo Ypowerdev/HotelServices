1ea634bdaa6e3f5dd3c192bd41da71783b52f606
Yury Videnin <yuryhhp@gmail.com>
2023-08-29 18:08:50 +0300

    changed Telegram.php

--- app/Helpers/Telegram.php
+++ app/Helpers/Telegram.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Http;
 
 class Telegram { 
 
-    const url = 'https://api.telegram.org/';
+    const URL = 'https://api.telegram.org/';
 
     protected $http;
     protected $bot;
 
-    public function __construct(Http $http, $bot)
+    public function __construct($bot)
     {
-        $this->http = $http;
         $this->bot = $bot; 
     }
 
     public function sendMessage($chat_id, $message) { 
         
-        $this->http::post(self::url.$this->bot.'/sendMessage', [
+        Http::post(self::URL.$this->bot.'/sendMessage', [
             'chat_id' => $chat_id,           
             'text' => $message,            
             'parse_mode' => 'html'
